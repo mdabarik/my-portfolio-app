7a3ece3c4ecd9d917ec3a4dd6a2aefc3e7d3e8ed
mdabarik <mdabarik19@gmail.com>
2023-12-12 04:28:41 +0600

    skills added

--- src/components/TechnicalSkills/TechnicalSkills.jsx
+++ src/components/TechnicalSkills/TechnicalSkills.jsx
@@ -1,16 +1,92 @@
 import { Container } from '@mui/material';
-import React from 'react';
-import Html from '../Logos/Node';
+import HtmlLogo from "./../../assets/skills/html.png";
+import CssLogo from "./../../assets/skills/css.png";
+import TailwindLogo from "./../../assets/skills/tailwind.png";
+import MuiLogo from "./../../assets/skills/material-ui.png";
+import JsLogo from "./../../assets/skills/js.png";
+import ReactLogo from "./../../assets/skills/reactjs.png";
+import NodejsLogo from "./../../assets/skills/nodejs.png";
+import ExpressLogo from "./../../assets/skills/express.png";
+import FirebaseLogo from "./../../assets/skills/firebase.png";
+import GitLogo from "./../../assets/skills/git.png";
+import GitHubLogo from "./../../assets/skills/github.png";
+import MongoDbLogo from "./../../assets/skills/mongodb.png";
+import CLogo from "./../../assets/skills/cprogramming.png";
+import JavaLogo from "./../../assets/skills/java.png";
+import PythonLogo from "./../../assets/skills/python.png";
+import PostGreSQLLogo from "./../../assets/skills/postgresql.png";
+
 
 const TechnicalSkills = () => {
     return (
-        <div>
+        <div className='my-10'>
             <Container maxWidth="xl">
-                <h2 className="text-3xl font-bold text-center">Technical <span className="text-[red]">Skills</span></h2>
-                <div>
-                    Technical Skills
-
-                    {/* <Html></Html> */}
+                <h2 className="text-3xl font-bold text-center mb-8">Technical <span className="text-[red]">Skills</span></h2>
+                <div className='flex flex-wrap gap-6 items-center justify-center'>
+                    <div className='bg-[#ff6f0130] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={HtmlLogo} alt="html logo" />
+                        <span className='text-center text-[10px] block'>html</span>
+                    </div>
+                    <div className='bg-[#0278bd30] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={CssLogo} alt="css logo" />
+                        <span className='text-center text-[10px] block'>css</span>
+                    </div>
+                    <div className='bg-[#05acc130] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={TailwindLogo} alt="tailwind logo" />
+                        <span className='text-center text-[10px] block'>tailwind css</span>
+                    </div>
+                    <div className='bg-[#0088d130] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={MuiLogo} alt="mui logo" />
+                        <span className='text-center text-[10px] block'>mui</span>
+                    </div>
+                    <div className='bg-[#fed70350] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={JsLogo} alt="js logo" />
+                        <span className='text-center text-[10px] block'>javascript</span>
+                    </div>
+                    <div className='bg-[#3977bd35] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={ReactLogo} alt="reactjs logo" />
+                        <span className='text-center text-[10px] block'>react js</span>
+                    </div>
+                    <div className='bg-[#21a36635] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={NodejsLogo} alt="nodejs logo" />
+                        <span className='text-center text-[10px] block'>node js</span>
+                    </div>
+                    <div className='bg-[#00000038] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={ExpressLogo} alt="exress logo" />
+                        <span className='text-center text-[10px] block'>express js</span>
+                    </div>
+                    <div className='bg-[#ffc40345] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={FirebaseLogo} alt="firebase logo" />
+                        <span className='text-center text-[10px] block'>firebase</span>
+                    </div>
+                    <div className='bg-[#f4511e35] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={GitLogo} alt="git logo" />
+                        <span className='text-center text-[10px] block'>git</span>
+                    </div>
+                    <div className='bg-[#00000030] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={GitHubLogo} alt="github logo" />
+                        <span className='text-center text-[10px] block'>github</span>
+                    </div>
+                    <div className='bg-[#4caf5030] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={MongoDbLogo} alt="mongodb logo" />
+                        <span className='text-center text-[10px] block'>mongodb</span>
+                    </div>
+                    <div className='bg-[#565f9f30] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={CLogo} alt="c logo" />
+                        <span className='text-center text-[10px] block'>c programming</span>
+                    </div>
+                    <div className='bg-[#0277bd30] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={JavaLogo} alt="java logo" />
+                        <span className='text-center text-[10px] block'>java</span>
+                    </div>
+                    <div className='bg-[#ffc00830] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={PythonLogo} alt="python logo" />
+                        <span className='text-center text-[10px] block'>python</span>
+                    </div>
+                    <div className='bg-[#0277bd30] w-fit p-2 rounded-lg'>
+                        <img className='w-[100px]' src={PostGreSQLLogo} alt="postgresql logo" />
+                        <span className='text-center text-[10px] block'>postgresql</span>
+                    </div>
                 </div>
             </Container>
         </div>
