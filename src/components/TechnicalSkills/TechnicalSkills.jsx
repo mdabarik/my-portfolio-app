a892720705ffc7e2190cbb87e2ebfaea5292fa48
mdabarik <mdabarik19@gmail.com>
2023-12-14 08:45:08 +0600

    animation added

--- src/components/TechnicalSkills/TechnicalSkills.jsx
+++ src/components/TechnicalSkills/TechnicalSkills.jsx
@@ -1,4 +1,3 @@
-import { Container } from '@mui/material';
 import HtmlLogo from "./../../assets/skills/html.png";
 import CssLogo from "./../../assets/skills/css.png";
 import TailwindLogo from "./../../assets/skills/tailwind.png";
@@ -16,7 +15,8 @@ import JavaLogo from "./../../assets/skills/java.png";
 import PythonLogo from "./../../assets/skills/python.png";
 import PostGreSQLLogo from "./../../assets/skills/postgresql.png";
 import VsCodeLogo from "./../../assets/skills/vscode.png";
-
+import Tilt from 'react-parallax-tilt';
+import "./TechnicalSkills.css";
 
 const TechnicalSkills = () => {
     return (
@@ -24,74 +24,108 @@ const TechnicalSkills = () => {
             <div className='max-w-[1280px] mx-auto'>
                 <h2 className="text-3xl font-bold text-center mb-8">Technical <span className="text-[red]">Skills</span></h2>
                 <div className='flex flex-wrap gap-6 items-center justify-center'>
-                    <div className='bg-[#ff6f0130] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={HtmlLogo} alt="html logo" />
-                        <span className='text-center text-[10px] block'>html</span>
-                    </div>
-                    <div className='bg-[#0278bd30] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={CssLogo} alt="css logo" />
-                        <span className='text-center text-[10px] block'>css</span>
-                    </div>
-                    <div className='bg-[#05acc130] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={TailwindLogo} alt="tailwind logo" />
-                        <span className='text-center text-[10px] block'>tailwind css</span>
-                    </div>
-                    <div className='bg-[#0088d130] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={MuiLogo} alt="mui logo" />
-                        <span className='text-center text-[10px] block'>mui</span>
-                    </div>
-                    <div className='bg-[#fed70350] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={JsLogo} alt="js logo" />
-                        <span className='text-center text-[10px] block'>javascript</span>
-                    </div>
-                    <div className='bg-[#3977bd35] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={ReactLogo} alt="reactjs logo" />
-                        <span className='text-center text-[10px] block'>react js</span>
-                    </div>
-                    <div className='bg-[#21a36635] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={NodejsLogo} alt="nodejs logo" />
-                        <span className='text-center text-[10px] block'>node js</span>
-                    </div>
-                    <div className='bg-[#00000038] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={ExpressLogo} alt="exress logo" />
-                        <span className='text-center text-[10px] block'>express js</span>
-                    </div>
-                    <div className='bg-[#4caf5030] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={MongoDbLogo} alt="mongodb logo" />
-                        <span className='text-center text-[10px] block'>mongodb</span>
-                    </div>
-                    <div className='bg-[#ffc40345] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={FirebaseLogo} alt="firebase logo" />
-                        <span className='text-center text-[10px] block'>firebase</span>
-                    </div>
-                    <div className='bg-[#f4511e35] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={GitLogo} alt="git logo" />
-                        <span className='text-center text-[10px] block'>git</span>
-                    </div>
-                    <div className='bg-[#00000030] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={GitHubLogo} alt="github logo" />
-                        <span className='text-center text-[10px] block'>github</span>
-                    </div>
-                    <div className='bg-[#0088d130] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={VsCodeLogo} alt="vscode logo" />
-                        <span className='text-center text-[10px] block'>vs code</span>
-                    </div>
-                    <div className='bg-[#565f9f30] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={CLogo} alt="c logo" />
-                        <span className='text-center text-[10px] block'>c programming</span>
-                    </div>
-                    <div className='bg-[#0277bd30] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={JavaLogo} alt="java logo" />
-                        <span className='text-center text-[10px] block'>java</span>
-                    </div>
-                    <div className='bg-[#ffc00830] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={PythonLogo} alt="python logo" />
-                        <span className='text-center text-[10px] block'>python</span>
-                    </div>
-                    <div className='bg-[#0277bd30] w-fit p-2 rounded-lg'>
-                        <img className='w-[100px]' src={PostGreSQLLogo} alt="postgresql logo" />
-                        <span className='text-center text-[10px] block'>postgresql</span>
-                    </div>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='glow-animation bg-[#ff6f0130] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={HtmlLogo} alt="html logo" />
+                            <span className='text-center text-[10px] block'>html</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#0278bd30] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={CssLogo} alt="css logo" />
+                            <span className='text-center text-[10px] block'>css</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#05acc130] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={TailwindLogo} alt="tailwind logo" />
+                            <span className='text-center text-[10px] block'>tailwind css</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#0088d130] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={MuiLogo} alt="mui logo" />
+                            <span className='text-center text-[10px] block'>mui</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#fed70350] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={JsLogo} alt="js logo" />
+                            <span className='text-center text-[10px] block'>javascript</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#3977bd35] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={ReactLogo} alt="reactjs logo" />
+                            <span className='text-center text-[10px] block'>react js</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#21a36635] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={NodejsLogo} alt="nodejs logo" />
+                            <span className='text-center text-[10px] block'>node js</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#00000038] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={ExpressLogo} alt="exress logo" />
+                            <span className='text-center text-[10px] block'>express js</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#4caf5030] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={MongoDbLogo} alt="mongodb logo" />
+                            <span className='text-center text-[10px] block'>mongodb</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#ffc40345] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={FirebaseLogo} alt="firebase logo" />
+                            <span className='text-center text-[10px] block'>firebase</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#f4511e35] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={GitLogo} alt="git logo" />
+                            <span className='text-center text-[10px] block'>git</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#00000030] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={GitHubLogo} alt="github logo" />
+                            <span className='text-center text-[10px] block'>github</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#0088d130] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={VsCodeLogo} alt="vscode logo" />
+                            <span className='text-center text-[10px] block'>vs code</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#565f9f30] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={CLogo} alt="c logo" />
+                            <span className='text-center text-[10px] block'>c programming</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#0277bd30] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={JavaLogo} alt="java logo" />
+                            <span className='text-center text-[10px] block'>java</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#ffc00830] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={PythonLogo} alt="python logo" />
+                            <span className='text-center text-[10px] block'>python</span>
+                        </div>
+                    </Tilt>
+                    <Tilt className="hover:cursor-pointer">
+                        <div className='bg-[#0277bd30] w-fit p-2 rounded-lg'>
+                            <img className='w-[100px]' src={PostGreSQLLogo} alt="postgresql logo" />
+                            <span className='text-center text-[10px] block'>postgresql</span>
+                        </div>
+                    </Tilt>
                 </div>
             </div>
         </div>
