63277a947fe506769d32d375f0298f8a59144c4b
mdabarik <mdabarik19@gmail.com>
2023-12-14 16:21:04 +0600

    experience and certificates added

---
+++ src/components/ExperienceTimeline/ExperienceTimeline.jsx
@@ -0,0 +1,81 @@
+
+const ExperienceTimeline = () => {
+    return (
+        <div className="my-12 w-[90%] lg:max-w-[1280px] mx-auto">
+            <h2 className="text-3xl font-bold text-center my-10">Experience <span className="text-[red]"> And Certificates</span></h2>
+
+            <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+                <li>
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-start md:text-end mb-10">
+                        <time className="font-mono italic">Problem Solved 300+</time>
+                        <div className="text-lg font-black">GeeksForGeeks</div>
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end mb-10">
+                        <time className="font-mono italic">Problem Solved 400+</time>
+                        <div className="text-lg font-black">LeetCode</div>
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-start md:text-end mb-10">
+                        <time className="font-mono italic">Certificates</time>
+                        <div className="text-lg font-black">Educative.io</div>
+                        Grokking Coding Interview Patterns in Java
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end mb-10">
+                        <time className="font-mono italic">Certificates</time>
+                        <div className="text-lg font-black">Educative.io</div>
+                        Java for Programmers
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-start md:text-end mb-10">
+                        <time className="font-mono italic">Certificates</time>
+                        <div className="text-lg font-black">Educative.io</div>
+                        Grokking Dynamic Programming: A Deep Dive Using Java
+                    </div>
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end mb-10">
+                        <time className="font-mono italic">Certificates</time>
+                        <div className="text-lg font-black">Educative.io</div>
+                        The Complete Advanced Guide to CSS
+                    </div>
+                    <hr />
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default ExperienceTimeline;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import TechnicalSkills from "../../components/TechnicalSkills/TechnicalSkills";
 import ScrollToTop from "react-scroll-to-top";
 import { FaArrowUp } from "react-icons/fa";
 import Snowfall from "react-snowfall";
+import ExperienceTimeline from "../../components/ExperienceTimeline/ExperienceTimeline";
 
 
 const Home = () => {
@@ -23,6 +24,9 @@ const Home = () => {
             </div>
 
             <ContactMe></ContactMe>
+
+            <ExperienceTimeline></ExperienceTimeline>
+
             <Footer></Footer>
             <div className="fixed z-50">
                 <ScrollToTop smooth component={<FaArrowUp className="drop-shadow-xl text-[#231f6f] flex items-center justify-center w-full bg-[]  text-2xl" ></FaArrowUp>} />
