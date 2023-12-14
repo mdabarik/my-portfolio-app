8146b9e857bcd44ce587ce705d41a829b4d81eb7
mdabarik <mdabarik19@gmail.com>
2023-12-14 16:26:50 +0600

    bug fixed

--- src/components/ExperienceTimeline/ExperienceTimeline.jsx
+++ src/components/ExperienceTimeline/ExperienceTimeline.jsx
@@ -6,6 +6,17 @@ const ExperienceTimeline = () => {
 
             <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
                 <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end mb-10">
+                        <time className="font-mono italic">Programming Instructor (2019-current)</time>
+                        <div className="text-lg font-black">Udemy.com</div>
+                    </div>
+                    <hr />
+                </li>
+                <li>
                     <div className="timeline-middle">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
                     </div>
